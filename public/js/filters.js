f56ef5c12452c22aa767d33b5336459b6ccacc57
Hillary Skye <skyeinc9@gmail.com>
2016-02-12 14:39:17 -0700

    fixing filter

--- public/js/filters.js
+++ public/js/filters.js
@@ -5,8 +5,10 @@
 // })
 
 app.filter('FREE', function() {
+  console.log('in filter')
   return function(input) {
     if (input === 0) {
+      console.log('in if in filter')
     return 'FREE'
   } else { return '$' + input }
   }

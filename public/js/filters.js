708ba55be600b9903bd367ca6b142193b8c6ae2e
Hillary Skye <skyeinc9@gmail.com>
2016-02-12 13:31:12 -0700

    fixing filter for FREE

--- public/js/filters.js
+++ public/js/filters.js
@@ -7,7 +7,7 @@
 app.filter ('FREE', function () {
   console.log('free outside')
   return function (input) {
-    if (input === 0 || input === 0.0) {
+    if (input === '0' || input === '0.0') {
       console.log('FREE inside')
      return 'FREE'
     } else {

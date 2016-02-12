bcd9337fb87c262bd61a6128e7d336806018e614
Hillary Skye <skyeinc9@gmail.com>
2016-02-12 15:09:50 -0700

    fixing filter for FREE

--- public/js/filters.js
+++ public/js/filters.js
@@ -7,7 +7,7 @@
 app.filter('FREE', function() {
   console.log('in filter')
   return function(input) {
-    if (input === 0) {
+    if (input === '0' | input === 0) {
       console.log('in if in filter')
     return 'FREE'
   } else { return '$' + input }
